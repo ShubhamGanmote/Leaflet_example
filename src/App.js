08965d401622007d5832fe701b435f38a5c4d637
ShubhamGanmote <shubhamganmote@gmail.com>
2020-03-13 09:31:02 +0530

    added list container skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-import MapComponent from './components/MapComponent/MapComponent';
+// import MapComponent from './components/MapComponent/MapComponent';
+import MapLayout from './components/Layout/MapLayout';
 
 function App() {
 	return (
 		<div className='App'>
-			<MapComponent />
+			<MapLayout />
 		</div>
 	);
 }
---
+++ src/components/ListContainer/ListContainer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import './ListContainer.css';
+
+const trimString = string => {
+	if (string.length === 0) {
+		return false;
+	} else {
+		return string.substr(0, 10) + '...';
+	}
+};
+
+const ListContainer = props => {
+	const searchResults = props.searchResults.map(result => {
+		return (
+			<li key={result.id}>
+				<div>
+					<div>
+						<span className='image-span-class'></span>
+					</div>
+					<div>
+						<span>{trimString(result.label)}</span>
+						<span>2,834sq ft</span>
+						<span>$302k</span>
+						<span>Office</span>
+					</div>
+				</div>
+			</li>
+		);
+	});
+
+	return (
+		<React.Fragment>
+			<div className='search-list-container'>
+				<span className='search-heading'>Search Results</span>
+				<div className='search-results-container'>
+					<ul className='search-results-list'>{searchResults}</ul>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default ListContainer;

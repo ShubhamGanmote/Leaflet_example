4b2014258f3e33c59b49c28e22a89580e6342263
ShubhamGanmote <shubham.ganmote@cognitiveclouds.com>
2020-03-16 11:53:32 +0530

    added list container

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'leaflet/dist/leaflet.css';
+import 'primereact/resources/primereact.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
